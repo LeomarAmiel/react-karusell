a9777ddfe7b583200ea3d3c47d3f1f506ca3e2af
Leomar Alejar <leomaramieldev@gmail.com>
2018-04-21 16:39:10 +0800

    Destructure so slide accepts children

--- src/App.js
+++ src/App.js
@@ -6,16 +6,58 @@ const Wrapper = styled.div`
     box-sizing: border-box;
     display: flex;
     justify-content: center;
-    overflow: hidden;
-    height: 240px;
+    overflow-x: hidden;
+    position: relative;
+    height: 420px;
+    width: 800px;
+    @media(max-width: 768px){
+        width: 100%;
+        height: 168px;
+    }
+`;
+
+const TextWrapper = styled.div`
+    background: rgba(0, 0, 0, 0.9);
+    border-radius: 10px;
+    padding: 20px;
+    color: white;
+    position: absolute;
+    left: 80px;
+    right: 80px;
+    bottom: 40px;
+
+    @media(max-width: 768px) {
+        display: none;
+    }
+`;
+
+const Headline = styled.h4`
+    margin: 0;
+    font-size: 14px;
+`;
+
+const Description = styled.p`
+    margin: 0;
+    font-size: 12px;
+`;
+
+const ImageWrapper = styled.div`
+    position: relative;
+    flex: 0 0 100%;
+    height: 420px;
+
+    @media(max-width: 768px){
+        width: 100%;
+        height: 168px;
+    }
 `;
 
 const Image = styled.img`
-    height: 100%;
+    object-fit: fill;
     width: 100%;
-    touch-action: none
-    user-drag: none; 
+    touch-action: none;
     user-select: none;
+    user-drag: none;
 `;
 
 class App extends Component {
@@ -42,11 +84,23 @@ class App extends Component {
     render() {
         return (
             <Wrapper>
-                <Slider>
+                <Slider autoplay autoplaySpeed>
                     {
                         this.state.isLoaded 
-                            ? this.state.items.map((data, index) => {
-                                    return <Image key={index} src={data.images[0].tn} alt={data.metadesc}/>
+                            ? this.state.items.map((data, index)    => {
+                                    return (
+                                        <ImageWrapper key={index}>
+                                            <TextWrapper>
+                                                <Headline>
+                                                    {data.title}
+                                                </Headline>
+                                                <Description>
+                                                    {data.metadesc}
+                                                </Description>
+                                            </TextWrapper>
+                                            <Image src={data.images[0].tn} alt={data.metadesc}/>
+                                        </ImageWrapper>
+                                    )
                                 })
                             : null
                     }
